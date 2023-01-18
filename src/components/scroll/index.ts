755d713127af9e8806dd22eda219e24f4d46315c
grwadl <youngwwad@gmail.com>
2023-01-18 23:55:35 +0200

    added button animation and closing the menu after clicking

--- src/components/scroll/index.ts
+++ src/components/scroll/index.ts
@@ -4,6 +4,10 @@ export const scrollElements: Record<string, HTMLElement | null> = {
   contact: document.querySelector("#contact"),
 };
 
+export const navigationPanel = document.querySelector(
+  ".header"
+) as HTMLDivElement;
+
 const scrollTriggers = document.querySelectorAll(
   "[data-scroll-to]"
 ) as NodeListOf<HTMLElement>;
@@ -18,6 +22,8 @@ scrollTriggers.forEach((trigger) =>
       scrollElements[idOfElement as keyof typeof scrollElements];
 
     if (!scrollElement) return;
+
+    navigationPanel.classList.remove("nav-opened");
 
     scroll({
       top: scrollElement.offsetTop,
